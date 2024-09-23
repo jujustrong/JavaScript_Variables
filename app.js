20fd6d467e94661c9cdfb475fc1ae078dbb5e0d6
Julian Armstrong <julian.armstrong8@gmail.com>
2024-09-23 09:08:48 -0600

    Completed variable exercises

--- app.js
+++ app.js
@@ -3,8 +3,32 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 & 2
+const firstName = "Julian";
+let lastName = "Armstrong";
+var age = 29;
 
-// Exercise 2
+console.log(`My first name is ${firstName} and my last name is ${lastName} and my age is ${age}`);
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`We are using case sensitive ${language} that was created in ${createdYear}.`)
+
+// Exercise 4
+let price = 99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(`The price for this item is ${price}, it is on sale for ${15}% but unfortunately, there are ${stock} in stock`.)
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(`Book title: ${title} \nAuthor: ${author} \nPage count: ${pageCount} \nBookmark on page: ${bookmark} \nRead: {hasRead}`)
